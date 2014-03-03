0d0ff0452e1c2c3a16786e9b885870861fde921b
Jennifer Morgan <jennifer@webapp-uk.vps.bitfolk.com>
2014-03-03 23:28:54 +0000

    Query API setup

--- src/filmlist/.#name.clj
+++
@@ -1,1 +0,0 @@
-jennifer@webapp-uk.vps.bitfolk.com.23861:1382696717
--- src/filmlist/name.clj
+++ src/filmlist/name.clj
@@ -1,18 +1,33 @@
 (ns filmlist.name)
 
+;; http://deanclatworthy.com/imdb/?q=world+war&year=2013
+
 (def f "World.War.Z.2013.720p.BluRay.x264.YIFY.mp4")
 
-(def words (map #(.toLowerCase %)  (re-seq #"\w+" f)))
+(def f  "The.Last.Airbender.2010.1080p.BrRip.x264.YIFY.mp4")
 
 ;; Need to look up these words and exclude ones that are not proper words.
 ;; also need to determine if any of them are years
 (def years #{"2000" "2001" "2002" "2003" "2004" "2005" "2006" "2007" "2008" "2009" "2010" "2011" "2012" "2013" "2014"})
 
-(def dict   (into #{}  (.split (slurp "/home/jennifer/2of12inf.txt") "\\r\\n")))
+(def dict (into #{} (.split (slurp "/home/jennifer/2of12inf.txt") "\\r\\n")))
 
+(defn make-words [f]
+  (map #(.toLowerCase %)  (re-seq #"\w+" f)))
 
-(def look-up-words (filter #(not (nil? %)) (map #(dict %) words)))
+(defn look-up-words [words]
+  (filter #(not (nil? %)) (map #(dict %) words)))
 
-(def look-up-years (filter #(not (nil? %)) (map #(years %) words)))
+(defn look-up-years [words]
+  (filter #(not (nil? %)) (map #(years %) words)))
 
-(def search-str (clojure.string/join " " (flatten  (conj look-up-words look-up-years))))
+(defn query-api[f]
+  (let [q-api "http://deanclatworthy.com/imdb/?q="
+        words (make-words f)
+        title-words (look-up-words words)
+        str1 (clojure.string/join "+" title-words)
+        query (str q-api str1)
+        y (first  (look-up-years words))]
+    (if y
+      (str query "&year=" y)
+      query)))
